8a19fe0636580f9cc133c2864e598d89691abb9e
daniel <danielo.visage@gmail.com>
2023-11-19 17:32:47 +0100

    create basic card list

--- app/src/main/java/com/example/chatterkotlinfrontend/MainActivity.kt
+++ app/src/main/java/com/example/chatterkotlinfrontend/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.example.chatterkotlinfrontend.ui.theme.ChatterKotlinFrontendTheme
 
 class MainActivity : ComponentActivity() {
@@ -68,24 +72,31 @@ fun AppScaffold(){
             )
         },
         ) { innerPadding ->
-            Column(Modifier.padding(innerPadding).fillMaxSize()){
+            Column(Modifier.padding(innerPadding)){
                 PostList()
             }
     }
 }
 
 @Composable
-fun PostCard(){
-    Card(){
-        Text(text = "Post Card")
+fun PostCard(text: String){
+    Card(Modifier.size(width = 300.dp, height = 300.dp).padding(16.dp)){
+        Text(text = text,
+            modifier = Modifier.padding(16.dp))
     }
 }
 
 @Composable
 fun PostList(){
-    Column(){
-        PostCard()
-        PostCard()
+    Column(
+        modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        PostCard("Card 0")
+        PostCard("Card 1")
+        PostCard("Card 2")
+        PostCard("Card 3")
     }
 
 }
